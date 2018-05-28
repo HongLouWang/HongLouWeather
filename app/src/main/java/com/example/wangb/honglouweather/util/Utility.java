16c3a18f1b9702677c4e5ff14516906eaccd45de
HongLouWang <1033745433@qq.com>
2018-05-28 12:51:05 -0700

    Add UI

--- app/src/main/java/com/example/wangb/honglouweather/util/Utility.java
+++ app/src/main/java/com/example/wangb/honglouweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.wangb.honglouweather.db.City;
 import com.example.wangb.honglouweather.db.County;
 import com.example.wangb.honglouweather.db.Province;
+import com.example.wangb.honglouweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -97,6 +99,22 @@ public class Utility
              }
          }
          return false;
+     }
+
+     public static Weather handleweatherResponse(String response)
+     {
+         try
+         {
+             JSONObject jsonObject = new JSONObject(response);
+             JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+             String weatherContent = jsonArray.getJSONObject(0).toString();
+             return new Gson().fromJson(weatherContent, Weather.class);
+         }
+         catch (Exception e)
+         {
+             e.printStackTrace();
+         }
+         return null;
      }
 
 }
